2114688e7631c1f42e0896168a5cd4d2a51a2886
Mahbub Ul Islam <mahbub_ul_islam@outlook.com>
2020-11-26 06:49:17 +0600

    dashboard added

--- accounts/views.py
+++ accounts/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'accounts/main.html')
+    return render(request, 'accounts/dashboard.html')
 
 def products(request):
     return render(request, 'accounts/products.html')
