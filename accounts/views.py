2feff3c74670d4ea17acffbb37890952414fb653
Mahbub Ul Islam <mahbub_ul_islam@outlook.com>
2020-12-01 22:32:41 +0600

    User creation form added

--- accounts/views.py
+++ accounts/views.py
@@ -4,8 +4,10 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
 
+
+
 from .models import *
-from .forms import OrderForm
+from .forms import OrderForm, CreateUserForm
 from .filters import OrderFilter
 
 
@@ -86,3 +88,22 @@ def deleteOrder(request, pk):
 
 
 
+
+
+def registerPage(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            
+            
+    
+    context = {'form':form}
+    return render(request, 'accounts/register.html', context)
+
+def loginPage(request):
+
+    context = {}
+    return render(request, 'accounts/login.html')
