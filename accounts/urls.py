dcc56dd9553da371f6f21615ecd7a5a5c46b91eb
Mahbub Ul Islam <mahbub_ul_islam@outlook.com>
2020-12-11 20:33:06 +0600

    password reset email added

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
@@ -18,6 +19,32 @@ urlpatterns = [
 
     path('register/', views.registerPage, name="register"),
     path('login/', views.loginPage, name="login"),
-    path('logout', views.logoutUser, name='logout'),
+    path('logout', views.logoutUser, name="logout"),
+
+
+    path('reset_password/',
+     auth_views.PasswordResetView.as_view(),
+     name="reset_password"),
+
+    path('reset_password_sent/', 
+        auth_views.PasswordResetDoneView.as_view(), 
+        name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/',
+     auth_views.PasswordResetConfirmView.as_view(), 
+     name="password_reset_confirm"),
+
+    path('reset_password_complete/', 
+        auth_views.PasswordResetCompleteView.as_view(), 
+        name="password_reset_complete"),
 ]
 
+
+
+
+'''
+accounts/password_reset/ [name='password_reset']
+accounts/password_reset/done/ [name='password_reset_done']
+accounts/reset/<uidb64>/<token>/ [name='password_reset_confirm']
+accounts/reset/done/ [name='password_reset_complete']
+'''
--- crm/settings.py
+++ crm/settings.py
@@ -133,3 +133,15 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images/profile_pic')
+
+
+
+# SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = 'mahbub77468@gmail.com'
+EMAIL_HOST_PASSWORD = "MahBub420$"
