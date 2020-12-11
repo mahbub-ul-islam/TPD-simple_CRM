a9fdbda01c4498707b77c39616b64ef5f78f65fa
Mahbub Ul Islam <mahbub_ul_islam@outlook.com>
2020-12-11 18:30:04 +0600

    signal added to accounts settings

--- accounts/signals.py
+++ accounts/signals.py
@@ -3,9 +3,14 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.contrib.auth.models import Group
 
+from django.dispatch import receiver
+
+
 from .models import Customer
 
 
+
+@receiver(post_save, sender=User)
 def customer_profile(sender, instance, created, **kwargs):
     if created:
         group = Group.objects.get(name='customer')
@@ -19,11 +24,15 @@ def customer_profile(sender, instance, created, **kwargs):
 
         print('Profile Created!')
     
-post_save.connect(customer_profile, sender=User)
+#post_save.connect(customer_profile, sender=User)
 
 
+@receiver(post_save, sender=User)
+def update_profile(sender, instance, created, **kwargs):
 
-
+    if created == False:
+        instance.customer.save()
+        print('profile updated')
 
 
 
--- accounts/views.py
+++ accounts/views.py
@@ -52,19 +52,7 @@ def userPage(request):
 
 
 
-@login_required(login_url='login')
-@allowed_users(allowed_roles=['customer'])
-def accountSettings(request):
-    customer = request.user.customer
-    form = CustomerForm(instance=customer)
 
-    if request.method == 'POST':
-        form = CustomerForm(request.POST, request.FILES, instance=customer)
-        if form.is_valid():
-            form.save()
-    
-    context = {'form':form}
-    return render(request, 'accounts/account_settings.html', context)
 
 
 
@@ -197,7 +185,19 @@ def logoutUser(request):
 
 
 
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['customer'])
+def accountSettings(request):
+    customer = request.user.customer
+    form = CustomerForm(instance=customer)
 
+    if request.method == 'POST':
+        form = CustomerForm(request.POST, request.FILES, instance=customer)
+        if form.is_valid():
+            user = form.save()
+    
+    context = {'form':form}
+    return render(request, 'accounts/account_settings.html', context)
 
 
 
